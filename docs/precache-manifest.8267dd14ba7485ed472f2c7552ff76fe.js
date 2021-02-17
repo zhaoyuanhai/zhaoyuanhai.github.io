c65967d82659fe3bedb9c10087c15170f9f10820
Yuanhai Zhao <yuanhai.zhao@capgemini.com>
2021-02-17 11:45:13 +0800

    add docs

---
+++ docs/precache-manifest.8267dd14ba7485ed472f2c7552ff76fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7aa94a0c6f7c2d0f93",
+    "url": "/css/app.baca5e14.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "520a0134792aeece829bf64e287a3f55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3436ea3ac82482f3e3a0",
+    "url": "/js/about.063687bd.js"
+  },
+  {
+    "revision": "1d7aa94a0c6f7c2d0f93",
+    "url": "/js/app.e2129824.js"
+  },
+  {
+    "revision": "25bff70239502279d9ca",
+    "url": "/js/chunk-vendors.2b241e03.js"
+  },
+  {
+    "revision": "6b9f1c52a11d2fe2f4a3cee497f9d5ff",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
